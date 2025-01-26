02853d8372f026f6647619bdfc9115def480933c
Stminty <peter.gannon96@gmail.com>
2025-01-26 19:00:50 +0000

    Removed sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import sentry from '@sentry/astro';
+
 import spotlightjs from '@spotlightjs/astro';
 
 // https://astro.build/config
@@ -16,7 +16,6 @@ export default defineConfig({
       sitemap(),
       react(),
       tailwind(),
-      sentry(),
       spotlightjs(),
     ],
 });
